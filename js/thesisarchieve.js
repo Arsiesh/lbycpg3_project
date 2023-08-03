c90d9915dc3216b0c73ba4b91df668cc3e519e63
Arsiesh <raphaelsacdalan@yahoo.com.ph>
2023-08-03 15:47:56 +0800

    added thesis archive

---
+++ js/thesisarchieve.js
@@ -0,0 +1,146 @@
+var x2013 = document.getElementById("1528499636284-970e5248-e484");
+var x2012 = document.getElementById("1528499636302-07f63ea1-28d0");
+var x2011 = document.getElementById("1528499736461-d67feb05-b568");
+var x2010 = document.getElementById("1528499774678-5d77c5dc-9316");
+var x2009 = document.getElementById("1528499911916-542a87ad-fce3");
+var x2008 = document.getElementById("1528499950659-2aa42149-de9a");
+var x2007 = document.getElementById("1528499986963-2e194dae-4921");
+
+var y2013 = document.getElementById("a2013");
+var y2012 = document.getElementById("a2012");
+var y2011 = document.getElementById("a2011");
+var y2010 = document.getElementById("a2010");
+var y2009 = document.getElementById("a2009");
+var y2008 = document.getElementById("a2008");
+var y2007 = document.getElementById("a2007");
+
+function show2013() {
+
+
+    x2013.className = "vc_tta-panel vc_active";
+    y2013.className = "vc_tta-tab vc_active";
+
+    x2012.className = "vc_tta-panel";
+    y2011.className = "vc_tta-tab";
+    x2011.className = "vc_tta-panel";
+    y2010.className = "vc_tta-tab";
+    x2010.className = "vc_tta-panel";
+    y2009.className = "vc_tta-tab";
+    x2009.className = "vc_tta-panel";
+    y2008.className = "vc_tta-tab";
+    x2008.className = "vc_tta-panel";
+    y2007.className = "vc_tta-tab";
+    x2007.className = "vc_tta-panel";
+
+
+}
+
+
+function show2012() {
+    x2012.className = "vc_tta-panel vc_active";
+    y2012.className = "vc_tta-tab vc_active";
+
+    x2013.className = "vc_tta-panel";
+    y2013.className = "vc_tta-tab";
+    x2011.className = "vc_tta-panel";
+    y2011.className = "vc_tta-tab";
+    x2010.className = "vc_tta-panel";
+    y2010.className = "vc_tta-tab";
+    x2009.className = "vc_tta-panel";
+    y2009.className = "vc_tta-tab";
+    x2008.className = "vc_tta-panel";
+    y2008.className = "vc_tta-tab";
+    x2007.className = "vc_tta-panel";
+    y2007.className = "vc_tta-tab";
+}
+
+function show2011() {
+    x2011.className = "vc_tta-panel vc_active";
+    y2011.className = "vc_tta-tab vc_active";
+
+    x2013.className = "vc_tta-panel";
+    y2013.className = "vc_tta-tab";
+    x2012.className = "vc_tta-panel";
+    y2012.className = "vc_tta-tab";
+    x2010.className = "vc_tta-panel";
+    y2010.className = "vc_tta-tab";
+    x2009.className = "vc_tta-panel";
+    y2009.className = "vc_tta-tab";
+    x2008.className = "vc_tta-panel";
+    y2008.className = "vc_tta-tab";
+    x2007.className = "vc_tta-panel";
+    y2007.className = "vc_tta-tab";
+}
+
+function show2010() {
+    x2010.className = "vc_tta-panel vc_active";
+    y2010.className = "vc_tta-tab vc_active";
+
+    x2013.className = "vc_tta-panel";
+    y2013.className = "vc_tta-tab";
+    x2012.className = "vc_tta-panel";
+    y2012.className = "vc_tta-tab";
+    x2011.className = "vc_tta-panel";
+    y2011.className = "vc_tta-tab";
+    x2009.className = "vc_tta-panel";
+    y2009.className = "vc_tta-tab";
+    x2008.className = "vc_tta-panel";
+    y2008.className = "vc_tta-tab";
+    x2007.className = "vc_tta-panel";
+    y2007.className = "vc_tta-tab";
+}
+
+function show2009() {
+    x2009.className = "vc_tta-panel vc_active";
+    y2009.className = "vc_tta-tab vc_active";
+
+    x2013.className = "vc_tta-panel";
+    y2013.className = "vc_tta-tab";
+    x2012.className = "vc_tta-panel";
+    y2012.className = "vc_tta-tab";
+    x2011.className = "vc_tta-panel";
+    y2011.className = "vc_tta-tab";
+    x2010.className = "vc_tta-panel";
+    y2010.className = "vc_tta-tab";
+    x2008.className = "vc_tta-panel";
+    y2008.className = "vc_tta-tab";
+    x2007.className = "vc_tta-panel";
+    y2007.className = "vc_tta-tab";
+}
+
+function show2008() {
+    x2008.className = "vc_tta-panel vc_active";
+    y2008.className = "vc_tta-tab vc_active";
+
+    x2013.className = "vc_tta-panel";
+    y2013.className = "vc_tta-tab";
+    x2012.className = "vc_tta-panel";
+    y2012.className = "vc_tta-tab";
+    x2011.className = "vc_tta-panel";
+    y2011.className = "vc_tta-tab";
+    x2010.className = "vc_tta-panel";
+    y2010.className = "vc_tta-tab";
+    x2009.className = "vc_tta-panel";
+    y2009.className = "vc_tta-tab";
+    x2007.className = "vc_tta-panel";
+    y2007.className = "vc_tta-tab";
+}
+
+function show2007() {
+    x2007.className = "vc_tta-panel vc_active";
+    y2007.className = "vc_tta-tab vc_active";
+
+    x2013.className = "vc_tta-panel";
+    y2013.className = "vc_tta-tab";
+    x2012.className = "vc_tta-panel";
+    y2012.className = "vc_tta-tab";
+    x2011.className = "vc_tta-panel";
+    y2011.className = "vc_tta-tab";
+    x2010.className = "vc_tta-panel";
+    y2010.className = "vc_tta-tab";
+    x2009.className = "vc_tta-panel";
+    y2009.className = "vc_tta-tab";
+    x2008.className = "vc_tta-panel";
+    y2008.className = "vc_tta-tab";
+}
+
